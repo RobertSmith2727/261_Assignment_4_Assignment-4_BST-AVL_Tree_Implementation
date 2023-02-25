cc63fe064e7631ae52f2d7e602a1e6360676eb4d
robertsmith2727 <smithro8@oregonstate.edu>
2023-02-24 16:00:12 -0800

    1

--- avl.py
+++ avl.py
@@ -153,8 +153,8 @@ class AVL(BST):
             return True
         # two subtrees
         if remove_node.left is not None and remove_node.right is not None:
-            self._remove_two_subtrees(remove_parent, remove_node)
-            self._remove_rebalance(remove_node)
+            successor_parent = self._remove_two_subtrees(remove_parent, remove_node)
+            self._remove_rebalance(successor_parent)
             return True
         else:
             return False
@@ -172,19 +172,20 @@ class AVL(BST):
         """
         # calls finds successor and parent
         successor, successor_parent = self.find_successor(remove_node)
-
+        if successor_parent is None:
+            successor_parent = successor
         # if root
         if self._root == remove_node:
             # if root successor is roots right node
             if self._root.right == successor:
                 self._root.value = successor.value
                 self._root.right = successor.right
-                return
+                return successor_parent
             if self._root.right != successor:
                 self._root.value = successor.value
                 successor_parent.left = successor.right
                 successor.right = None
-            return
+            return successor_parent
         # assigns remove left to successor left
         successor.left = remove_node.left
         if successor != remove_node.right:
@@ -196,7 +197,8 @@ class AVL(BST):
             remove_parent.right = successor
         else:
             remove_parent.left = successor
-        return
+        return successor_parent
+
 
     def _remove_rebalance(self, parent_node: AVLNode):
         while parent_node is not None:
@@ -341,42 +343,42 @@ if __name__ == '__main__':
     #         raise Exception("PROBLEM WITH ADD OPERATION")
     # print('add() stress test finished')
     print("\nPDF - method remove() example 1")
-    print("-------------------------------")
-    test_cases = (
-        ((1, 2, 3), 2),  # no AVL rotation
-        ((1, 2, 3), 1),  # no AVL rotation
-        ((1, 2, 3), 3),  # no AVL rotation
-        ((50, 40, 60, 30, 70, 20, 80, 45), 0),
-        ((50, 40, 60, 30, 70, 20, 80, 45), 45),  # no AVL rotation
-        ((50, 40, 60, 30, 70, 20, 80, 45), 40),  # no AVL rotation
-        ((50, 40, 60, 30, 70, 20, 80, 45), 30),  # no AVL rotation
-    )
-    for case, del_value in test_cases:
-        tree = AVL(case)
-        print('INPUT  :', tree, "DEL:", del_value)
-        tree.remove(del_value)
-        print('RESULT :', tree)
-    print("\nPDF - method remove() example 2")
-    print("-------------------------------")
-    test_cases = (
-        ((50, 40, 60, 30, 70, 20, 80, 45), 20),  # RR
-        ((50, 40, 60, 30, 70, 20, 80, 15), 40),  # LL
-        ((50, 40, 60, 30, 70, 20, 80, 35), 20),  # RL
-        ((50, 40, 60, 30, 70, 20, 80, 25), 40),  # LR
-    )
-    for case, del_value in test_cases:
-        tree = AVL(case)
-        print('INPUT  :', tree, "DEL:", del_value)
-        tree.remove(del_value)
-        print('RESULT :', tree)
-    print("\nPDF - method remove() example 3")
-    print("-------------------------------")
-    case = range(-9, 16, 2)
-    tree = AVL(case)
-    for del_value in case:
-        print('INPUT  :', tree, del_value)
-        tree.remove(del_value)
-        print('RESULT :', tree)
+    # print("-------------------------------")
+    # test_cases = (
+    #     ((1, 2, 3), 2),  # no AVL rotation
+    #     ((1, 2, 3), 1),  # no AVL rotation
+    #     ((1, 2, 3), 3),  # no AVL rotation
+    #     ((50, 40, 60, 30, 70, 20, 80, 45), 0),
+    #     ((50, 40, 60, 30, 70, 20, 80, 45), 45),  # no AVL rotation
+    #     ((50, 40, 60, 30, 70, 20, 80, 45), 40),  # no AVL rotation
+    #     ((50, 40, 60, 30, 70, 20, 80, 45), 30),  # no AVL rotation
+    # )
+    # for case, del_value in test_cases:
+    #     tree = AVL(case)
+    #     print('INPUT  :', tree, "DEL:", del_value)
+    #     tree.remove(del_value)
+    #     print('RESULT :', tree)
+    # print("\nPDF - method remove() example 2")
+    # print("-------------------------------")
+    # test_cases = (
+    #     ((50, 40, 60, 30, 70, 20, 80, 45), 20),  # RR
+    #     ((50, 40, 60, 30, 70, 20, 80, 15), 40),  # LL
+    #     ((50, 40, 60, 30, 70, 20, 80, 35), 20),  # RL
+    #     ((50, 40, 60, 30, 70, 20, 80, 25), 40),  # LR
+    # )
+    # for case, del_value in test_cases:
+    #     tree = AVL(case)
+    #     print('INPUT  :', tree, "DEL:", del_value)
+    #     tree.remove(del_value)
+    #     print('RESULT :', tree)
+    # print("\nPDF - method remove() example 3")
+    # print("-------------------------------")
+    # case = range(-9, 16, 2)
+    # tree = AVL(case)
+    # for del_value in case:
+    #     print('INPUT  :', tree, del_value)
+    #     tree.remove(del_value)
+    #     print('RESULT :', tree)
     print("\nPDF - method remove() example 4")
     print("-------------------------------")
     case = range(0, 34, 3)
@@ -386,71 +388,71 @@ if __name__ == '__main__':
         print('INPUT  :', tree, root_value)
         tree.remove(root_value)
         print('RESULT :', tree)
-    print("\nPDF - method remove() example 5")
-    print("-------------------------------")
-    for _ in range(100):
-        case = list(set(random.randrange(1, 20000) for _ in range(900)))
-        tree = AVL(case)
-        for value in case[::2]:
-            tree.remove(value)
-        if not tree.is_valid_avl():
-            raise Exception("PROBLEM WITH REMOVE OPERATION")
-    print('remove() stress test finished')
-    print("\nPDF - method contains() example 1")
-    print("---------------------------------")
-    tree = AVL([10, 5, 15])
-    print(tree.contains(15))
-    print(tree.contains(-10))
-    print(tree.contains(15))
-    print("\nPDF - method contains() example 2")
-    print("---------------------------------")
-    tree = AVL()
-    print(tree.contains(0))
-    print("\nPDF - method inorder_traversal() example 1")
-    print("---------------------------------")
-    tree = AVL([10, 20, 5, 15, 17, 7, 12])
-    print(tree.inorder_traversal())
-    print("\nPDF - method inorder_traversal() example 2")
-    print("---------------------------------")
-    tree = AVL([8, 10, -4, 5, -1])
-    print(tree.inorder_traversal())
-    print("\nPDF - method find_min() example 1")
-    print("---------------------------------")
-    tree = AVL([10, 20, 5, 15, 17, 7, 12])
-    print(tree)
-    print("Minimum value is:", tree.find_min())
-    print("\nPDF - method find_min() example 2")
-    print("---------------------------------")
-    tree = AVL([8, 10, -4, 5, -1])
-    print(tree)
-    print("Minimum value is:", tree.find_min())
-    print("\nPDF - method find_max() example 1")
-    print("---------------------------------")
-    tree = AVL([10, 20, 5, 15, 17, 7, 12])
-    print(tree)
-    print("Maximum value is:", tree.find_max())
-    print("\nPDF - method find_max() example 2")
-    print("---------------------------------")
-    tree = AVL([8, 10, -4, 5, -1])
-    print(tree)
-    print("Maximum value is:", tree.find_max())
-    print("\nPDF - method is_empty() example 1")
-    print("---------------------------------")
-    tree = AVL([10, 20, 5, 15, 17, 7, 12])
-    print("Tree is empty:", tree.is_empty())
-    print("\nPDF - method is_empty() example 2")
-    print("---------------------------------")
-    tree = AVL()
-    print("Tree is empty:", tree.is_empty())
-    print("\nPDF - method make_empty() example 1")
-    print("---------------------------------")
-    tree = AVL([10, 20, 5, 15, 17, 7, 12])
-    print("Tree before make_empty():", tree)
-    tree.make_empty()
-    print("Tree after make_empty(): ", tree)
-    print("\nPDF - method make_empty() example 2")
-    print("---------------------------------")
-    tree = AVL()
-    print("Tree before make_empty():", tree)
-    tree.make_empty()
-    print("Tree after make_empty(): ", tree)
+    # print("\nPDF - method remove() example 5")
+    # print("-------------------------------")
+    # for _ in range(100):
+    #     case = list(set(random.randrange(1, 20000) for _ in range(900)))
+    #     tree = AVL(case)
+    #     for value in case[::2]:
+    #         tree.remove(value)
+    #     if not tree.is_valid_avl():
+    #         raise Exception("PROBLEM WITH REMOVE OPERATION")
+    # print('remove() stress test finished')
+    # print("\nPDF - method contains() example 1")
+    # print("---------------------------------")
+    # tree = AVL([10, 5, 15])
+    # print(tree.contains(15))
+    # print(tree.contains(-10))
+    # print(tree.contains(15))
+    # print("\nPDF - method contains() example 2")
+    # print("---------------------------------")
+    # tree = AVL()
+    # print(tree.contains(0))
+    # print("\nPDF - method inorder_traversal() example 1")
+    # print("---------------------------------")
+    # tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    # print(tree.inorder_traversal())
+    # print("\nPDF - method inorder_traversal() example 2")
+    # print("---------------------------------")
+    # tree = AVL([8, 10, -4, 5, -1])
+    # print(tree.inorder_traversal())
+    # print("\nPDF - method find_min() example 1")
+    # print("---------------------------------")
+    # tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    # print(tree)
+    # print("Minimum value is:", tree.find_min())
+    # print("\nPDF - method find_min() example 2")
+    # print("---------------------------------")
+    # tree = AVL([8, 10, -4, 5, -1])
+    # print(tree)
+    # print("Minimum value is:", tree.find_min())
+    # print("\nPDF - method find_max() example 1")
+    # print("---------------------------------")
+    # tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    # print(tree)
+    # print("Maximum value is:", tree.find_max())
+    # print("\nPDF - method find_max() example 2")
+    # print("---------------------------------")
+    # tree = AVL([8, 10, -4, 5, -1])
+    # print(tree)
+    # print("Maximum value is:", tree.find_max())
+    # print("\nPDF - method is_empty() example 1")
+    # print("---------------------------------")
+    # tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    # print("Tree is empty:", tree.is_empty())
+    # print("\nPDF - method is_empty() example 2")
+    # print("---------------------------------")
+    # tree = AVL()
+    # print("Tree is empty:", tree.is_empty())
+    # print("\nPDF - method make_empty() example 1")
+    # print("---------------------------------")
+    # tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    # print("Tree before make_empty():", tree)
+    # tree.make_empty()
+    # print("Tree after make_empty(): ", tree)
+    # print("\nPDF - method make_empty() example 2")
+    # print("---------------------------------")
+    # tree = AVL()
+    # print("Tree before make_empty():", tree)
+    # tree.make_empty()
+    # print("Tree after make_empty(): ", tree)
